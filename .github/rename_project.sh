1a0a1b375b01c13c82f0763824869e9ba635e6a8
shriyanshagnihotri <shriyanshagnihotri@users.noreply.github.com>
2024-11-04 18:31:55 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="test-zeus-ai"
+original_name="hercules"
+original_urlname="hercules"
+original_description="Awesome hercules created by test-zeus-ai"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv hercules $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from hercules.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "hercules"
