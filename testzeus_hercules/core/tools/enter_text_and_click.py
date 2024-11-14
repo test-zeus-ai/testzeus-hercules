4257ee991fd00b999423b487fc1c28c876d76316
Shriyansh Agnihotri <shriyansh@testarmy.ai>
2024-11-15 02:42:10 +0530

    Adding skip in click on JS dialog

--- testzeus_hercules/core/tools/enter_text_and_click.py
+++ testzeus_hercules/core/tools/enter_text_and_click.py
@@ -41,13 +41,19 @@ async def enter_text_and_click(
         Optional[str],
         "The input response to a dialog box. THIS SHOULD NOT HAVE RANDOM VALUE BUT ONLY AS PER THE TEST INPUT",
     ],
-    expected_message_of_dialog: Annotated[Optional[str], "The expected message of the dialog box when it opens."],
+    expected_message_of_dialog: Annotated[
+        Optional[str], "The expected message of the dialog box when it opens."
+    ],
     action_on_dialog: Annotated[
         Optional[str],
         "The action to be performed on the dialog box. ONLY 'DISMISS' OR 'ACCEPT' AS A VALUE ALLOWED.",
     ],
-    wait_before_click_execution: Annotated[float, "Optional wait time in seconds before executing the click.", float] = 0.0,
-) -> Annotated[str, "A message indicating success or failure of the text entry and click."]:
+    wait_before_click_execution: Annotated[
+        float, "Optional wait time in seconds before executing the click.", float
+    ] = 0.0,
+) -> Annotated[
+    str, "A message indicating success or failure of the text entry and click."
+]:
     """
     Enters text into an element and then clicks on another element.
 
@@ -68,7 +74,9 @@ async def enter_text_and_click(
     await enter_text_and_click("[mmid='1234']", "Hello, World!", "[mmid='5678']", wait_before_click_execution=1.5)
     ```
     """
-    logger.info(f"Entering text '{text_to_enter}' into element with selector '{text_selector}' and then clicking element with selector '{click_selector}'.")
+    logger.info(
+        f"Entering text '{text_to_enter}' into element with selector '{text_selector}' and then clicking element with selector '{click_selector}'."
+    )
     add_event(EventType.INTERACTION, EventData(detail="enter_text_and_click"))
     # Initialize PlaywrightManager and get the active browser page
     browser_manager = PlaywrightManager()
@@ -82,26 +90,36 @@ async def enter_text_and_click(
             data = get_page_data(page)
             user_input_dialog_response = data.get("user_input_dialog_response")
             expected_message_of_dialog = data.get("expected_message_of_dialog")
-            action_on_dialog = data.get("action_on_dialog").strip().lower()
+            action_on_dialog = data.get("action_on_dialog", "")
+            if action_on_dialog:
+                action_on_dialog = action_on_dialog.lower().strip()
             logger.info(f"Dialog message: {dialog.message}")
 
             # Check if the dialog message matches the expected message (if provided)
-            if expected_message_of_dialog and dialog.message != expected_message_of_dialog:
-                logger.error(f"Dialog message does not match the expected message: {expected_message_of_dialog}")
+            if (
+                expected_message_of_dialog
+                and dialog.message != expected_message_of_dialog
+            ):
+                logger.error(
+                    f"Dialog message does not match the expected message: {expected_message_of_dialog}"
+                )
                 if action_on_dialog == "accept":
-                    await dialog.accept(user_input_dialog_response)
+                    if dialog.type == "prompt":
+                        await dialog.accept(user_input_dialog_response)
+                    else:
+                        await dialog.accept()
+                elif action_on_dialog == "dismiss":
+                    await dialog.dismiss()
                 else:
                     await dialog.dismiss()  # Dismiss if the dialog message doesn't match
-                return
-
-            if action_on_dialog == "accept":
+            elif user_input_dialog_response:
                 await dialog.accept(user_input_dialog_response)
             else:
                 await dialog.dismiss()
 
         except Exception as e:
             logger.info(f"Error handling dialog: {e}")
-            await dialog.accept(user_input_dialog_response)
+            # await dialog.accept(user_input_dialog_response)
 
     if page is None:  # type: ignore
         logger.error("No active page found")
@@ -112,7 +130,9 @@ async def enter_text_and_click(
     function_name = inspect.currentframe().f_code.co_name  # type: ignore
     await browser_manager.take_screenshots(f"{function_name}_start", page)
 
-    text_entry_result = await do_entertext(page, text_selector, text_to_enter, use_keyboard_fill=True)
+    text_entry_result = await do_entertext(
+        page, text_selector, text_to_enter, use_keyboard_fill=True
+    )
 
     # await browser_manager.notify_user(text_entry_result["summary_message"])
     if not text_entry_result["summary_message"].startswith("Success"):
@@ -133,9 +153,13 @@ async def enter_text_and_click(
 
     # if the text_selector is the same as the click_selector, press the Enter key instead of clicking
     if text_selector == click_selector:
-        do_press_key_combination_result = await do_press_key_combination(browser_manager, page, "Enter")
+        do_press_key_combination_result = await do_press_key_combination(
+            browser_manager, page, "Enter"
+        )
         if do_press_key_combination_result:
-            result["detailed_message"] += f' Instead of click, pressed the Enter key successfully on element: "{click_selector}".'
+            result[
+                "detailed_message"
+            ] += f' Instead of click, pressed the Enter key successfully on element: "{click_selector}".'
             await browser_manager.notify_user(
                 f'Pressed the Enter key successfully on element: "{click_selector}".',
                 message_type=MessageType.ACTION,
@@ -151,11 +175,15 @@ async def enter_text_and_click(
     else:
         await browser_manager.highlight_element(click_selector, True)
 
-        do_click_result = await do_click(page, click_selector, wait_before_click_execution)
+        do_click_result = await do_click(
+            page, click_selector, wait_before_click_execution
+        )
         result["detailed_message"] += f' {do_click_result["detailed_message"]}'
         # await browser_manager.notify_user(do_click_result["summary_message"])
 
-    await asyncio.sleep(0.1)  # sleep for 100ms to allow the mutation observer to detect changes
+    await asyncio.sleep(
+        0.1
+    )  # sleep for 100ms to allow the mutation observer to detect changes
 
     await browser_manager.take_screenshots(f"{function_name}_end", page)
 
